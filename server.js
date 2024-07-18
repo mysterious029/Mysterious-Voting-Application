8ab694a4391e225143a1b13b2c715fe55d11f96f
mysterious029 <mukeshmandal9783@gmail.com>
2024-07-18 15:52:05 +0530

    first

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const db = require('./db');
+require('dotenv').config();
+
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the Mysterious Voting System");
+})
+
+
+// Import the router files
+const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
+
+// Use the routers
+app.use('/user', userRoutes);
+app.use('/candidate', candidateRoutes);
+
+
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
