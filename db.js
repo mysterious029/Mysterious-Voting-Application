d54c5df4e684be666978e9e9a2f2a0abf99a2369
MUKESH KUMAR MANDAL <109544411+mysterious029@users.noreply.github.com>
2024-07-30 23:38:29 +0530

    update db.js

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Define the MongoDB connection URL
-const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
-// const mongoURL = process.env.MONGODB_URL;
+// const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
